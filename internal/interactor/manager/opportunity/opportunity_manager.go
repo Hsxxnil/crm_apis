6f947cff313a5bcd073544d09c8088f33ade927f
Hsxxnil <lhsuan1225@gmail.com>
2023-05-01 12:02:32 +0800

    fix: opportunities association

--- internal/interactor/manager/opportunity/opportunity_manager.go
+++ internal/interactor/manager/opportunity/opportunity_manager.go
@@ -67,13 +67,12 @@ func (m *manager) GetByList(input *opportunityModel.Fields) interface{} {
 		return code.GetCodeMessage(code.InternalServerError, err.Error())
 	}
 
-	for _, opportunities := range output.Opportunities {
-		opportunities.AccountName = *opportunities.Accounts.Name
-		opportunities.Accounts = nil
-		opportunities.CreatedBy = *opportunities.CreatedByUsers.Name
-		opportunities.CreatedByUsers = nil
-		opportunities.UpdatedBy = *opportunities.UpdatedByUsers.Name
-		opportunities.UpdatedByUsers = nil
+	for _, opportunitiesBase := range opportunityBase {
+		for _, opportunities := range output.Opportunities {
+			opportunities.AccountName = *opportunitiesBase.Accounts.Name
+			opportunities.CreatedBy = *opportunitiesBase.CreatedByUsers.Name
+			opportunities.UpdatedBy = *opportunitiesBase.UpdatedByUsers.Name
+		}
 	}
 
 	return code.GetCodeMessage(code.Successful, output)
@@ -98,12 +97,9 @@ func (m *manager) GetBySingle(input *opportunityModel.Field) interface{} {
 		return code.GetCodeMessage(code.InternalServerError, err)
 	}
 
-	output.AccountName = *output.Accounts.Name
-	output.Accounts = nil
-	output.CreatedBy = *output.CreatedByUsers.Name
-	output.CreatedByUsers = nil
-	output.UpdatedBy = *output.UpdatedByUsers.Name
-	output.UpdatedByUsers = nil
+	output.AccountName = *opportunityBase.Accounts.Name
+	output.CreatedBy = *opportunityBase.CreatedByUsers.Name
+	output.UpdatedBy = *opportunityBase.UpdatedByUsers.Name
 
 	return code.GetCodeMessage(code.Successful, output)
 }
