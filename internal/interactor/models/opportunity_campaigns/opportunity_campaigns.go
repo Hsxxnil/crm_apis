f29a21d9b04246170cc53b0138fd47d0e18e1771
Hsxxnil <lhsuan1225@gmail.com>
2023-05-03 02:09:28 +0800

    fix: #3 one opportunity <--> many campaigns

--- internal/interactor/models/opportunity_campaigns/opportunity_campaigns.go
+++ internal/interactor/models/opportunity_campaigns/opportunity_campaigns.go
@@ -81,3 +81,10 @@ type Update struct {
 	// 更新者
 	UpdatedBy *string `json:"updated_by,omitempty" binding:"required,uuid4" validate:"required,uuid4"`
 }
+
+type OpportunitySingle struct {
+	// 行銷活動ID
+	CampaignID string `json:"campaign_id,omitempty"`
+	// 行銷活動名稱
+	CampaignName string `json:"campaign_name,omitempty"`
+}
