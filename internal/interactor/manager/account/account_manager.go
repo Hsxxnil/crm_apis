6ef42f17acf8bb67ba9c44ab660c3c1ac0e44b41
Hsxxnil <lhsuan1225@gmail.com>
2023-05-03 17:36:17 +0800

    fix: account route

--- internal/interactor/manager/account/account_manager.go
+++ internal/interactor/manager/account/account_manager.go
@@ -17,7 +17,9 @@ import (
 type Manager interface {
 	Create(trx *gorm.DB, input *accountModel.Create) interface{}
 	GetByList(input *accountModel.Fields) interface{}
+	GetByListContacts(input *accountModel.Fields) interface{}
 	GetBySingle(input *accountModel.Field) interface{}
+	GetBySingleContacts(input *accountModel.Field) interface{}
 	Delete(input *accountModel.Field) interface{}
 	Update(input *accountModel.Update) interface{}
 }
@@ -76,6 +78,37 @@ func (m *manager) GetByList(input *accountModel.Fields) interface{} {
 	return code.GetCodeMessage(code.Successful, output)
 }
 
+func (m *manager) GetByListContacts(input *accountModel.Fields) interface{} {
+	output := &accountModel.ListContacts{}
+	output.Limit = input.Limit
+	output.Page = input.Page
+	quantity, accountBase, err := m.AccountService.GetByList(input)
+	if err != nil {
+		log.Error(err)
+		return code.GetCodeMessage(code.InternalServerError, err.Error())
+	}
+	output.Total.Total = quantity
+	accountByte, err := json.Marshal(accountBase)
+	if err != nil {
+		log.Error(err)
+		return code.GetCodeMessage(code.InternalServerError, err.Error())
+	}
+	output.Pages = util.Pagination(quantity, output.Limit)
+	err = json.Unmarshal(accountByte, &output.Accounts)
+	if err != nil {
+		log.Error(err)
+		return code.GetCodeMessage(code.InternalServerError, err.Error())
+	}
+
+	for i, accounts := range output.Accounts {
+		accounts.IndustryName = *accountBase[i].Industries.Name
+		accounts.CreatedBy = *accountBase[i].CreatedByUsers.Name
+		accounts.UpdatedBy = *accountBase[i].UpdatedByUsers.Name
+	}
+
+	return code.GetCodeMessage(code.Successful, output)
+}
+
 func (m *manager) GetBySingle(input *accountModel.Field) interface{} {
 	accountBase, err := m.AccountService.GetBySingle(input)
 	if err != nil {
@@ -88,6 +121,32 @@ func (m *manager) GetBySingle(input *accountModel.Field) interface{} {
 	}
 
 	output := &accountModel.Single{}
+	accountByte, _ := json.Marshal(accountBase)
+	err = json.Unmarshal(accountByte, &output)
+	if err != nil {
+		log.Error(err)
+		return code.GetCodeMessage(code.InternalServerError, err)
+	}
+
+	output.IndustryName = *accountBase.Industries.Name
+	output.CreatedBy = *accountBase.CreatedByUsers.Name
+	output.UpdatedBy = *accountBase.UpdatedByUsers.Name
+
+	return code.GetCodeMessage(code.Successful, output)
+}
+
+func (m *manager) GetBySingleContacts(input *accountModel.Field) interface{} {
+	accountBase, err := m.AccountService.GetBySingle(input)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return code.GetCodeMessage(code.DoesNotExist, err)
+		}
+
+		log.Error(err)
+		return code.GetCodeMessage(code.InternalServerError, err)
+	}
+
+	output := &accountModel.SingleContacts{}
 	accountByte, _ := json.Marshal(accountBase)
 	err = json.Unmarshal(accountByte, &output)
 	if err != nil {
--- internal/router/account/account_router.go
+++ internal/router/account/account_router.go
@@ -13,7 +13,9 @@ func GetRouter(router *gin.Engine, db *gorm.DB) *gin.Engine {
 	{
 		v10.POST("", middleware.Transaction(db), control.Create)
 		v10.GET("", control.GetByList)
+		v10.GET("contacts", control.GetByListContacts)
 		v10.GET(":accountID", control.GetBySingle)
+		v10.GET("contacts/:accountID", control.GetBySingleContacts)
 		v10.DELETE(":accountID", control.Delete)
 		v10.PATCH(":accountID", control.Update)
 	}
