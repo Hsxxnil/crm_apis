076da4055704af307781f6852f391afbffaf4a94
Hsxxnil <lhsuan1225@gmail.com>
2023-07-26 09:29:02 +0800

    fix: #77 event_user_attendee add GetByListNoQuantity

--- internal/entity/postgresql/event_user_attendee/event_user_attendee_entity.go
+++ internal/entity/postgresql/event_user_attendee/event_user_attendee_entity.go
@@ -13,6 +13,7 @@ type Entity interface {
 	WithTrx(trx *gorm.DB) Entity
 	Create(input *model.Base) (err error)
 	GetByList(input *model.Base) (quantity int64, output []*model.Table, err error)
+	GetByListNoQuantity(input *model.Base) (output []*model.Table, err error)
 	GetBySingle(input *model.Base) (output *model.Table, err error)
 	GetByQuantity(input *model.Base) (quantity int64, err error)
 	Delete(input *model.Base) (err error)
@@ -72,6 +73,21 @@ func (s *storage) GetByList(input *model.Base) (quantity int64, output []*model.
 	}
 
 	return quantity, output, nil
+}
+
+func (s *storage) GetByListNoQuantity(input *model.Base) (output []*model.Table, err error) {
+	query := s.db.Model(&model.Table{}).Preload(clause.Associations)
+	if input.EventUserAttendeeID != nil {
+		query.Where("event_user_attendee_id = ?", input.EventUserAttendeeID)
+	}
+
+	err = query.Order("created_at desc").Find(&output).Error
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	return output, nil
 }
 
 func (s *storage) GetBySingle(input *model.Base) (output *model.Table, err error) {
