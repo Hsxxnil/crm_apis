9d66117f3c1582130bb589b9785424ae5a388e7a
Hsxxnil <lhsuan1225@gmail.com>
2023-07-26 13:20:26 +0800

    fix: #78 contacts add is_deleted

--- internal/interactor/service/contact/contact_service.go
+++ internal/interactor/service/contact/contact_service.go
@@ -58,6 +58,7 @@ func (s *service) Create(input *model.Create) (output *db.Base, err error) {
 	base.UpdatedAt = util.PointerTime(util.NowToUTC())
 	base.UpdatedBy = util.PointerString(input.CreatedBy)
 	base.SalespersonID = util.PointerString(input.CreatedBy)
+	base.IsDeleted = util.PointerBool(false)
 	err = s.Repository.Create(base)
 	if err != nil {
 		log.Error(err)
@@ -201,7 +202,8 @@ func (s *service) Delete(input *model.Field) (err error) {
 	}
 
 	field.UpdatedAt = util.PointerTime(util.NowToUTC())
-	err = s.Repository.Delete(field)
+	field.IsDeleted = util.PointerBool(true)
+	err = s.Repository.Update(field)
 	if err != nil {
 		log.Error(err)
 		return err
---
+++ migrations/20230726051938_alter_contacts.up.sql
@@ -0,0 +1,2 @@
+alter table contacts
+    add is_deleted bool default false not null;
