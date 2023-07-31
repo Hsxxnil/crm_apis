02d891881a5a553bcdc501436b903ebeb490d625
Hsxxnil <lhsuan1225@gmail.com>
2023-07-31 16:41:30 +0800

    fix: #82 contacts is_deleted filter

--- internal/entity/postgresql/contact/contact_entity.go
+++ internal/entity/postgresql/contact/contact_entity.go
@@ -138,7 +138,7 @@ func (s *storage) GetByListNoPagination(input *model.Base) (output []*model.Tabl
 	}
 
 	if input.IsDeleted != nil {
-		query.Where("contacts.is_deleted = ?", input.IsDeleted)
+		query.Where("is_deleted = ?", input.IsDeleted)
 	}
 
 	err = query.Order("created_at desc").Find(&output).Error
@@ -157,7 +157,7 @@ func (s *storage) GetBySingle(input *model.Base) (output *model.Table, err error
 	}
 
 	if input.IsDeleted != nil {
-		query.Where("contacts.is_deleted = ?", input.IsDeleted)
+		query.Where("is_deleted = ?", input.IsDeleted)
 	}
 
 	err = query.First(&output).Error
@@ -176,7 +176,7 @@ func (s *storage) GetByQuantity(input *model.Base) (quantity int64, err error) {
 	}
 
 	if input.IsDeleted != nil {
-		query.Where("contacts.is_deleted = ?", input.IsDeleted)
+		query.Where("is_deleted = ?", input.IsDeleted)
 	}
 
 	err = query.Count(&quantity).Select("*").Error
