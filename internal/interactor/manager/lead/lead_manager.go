6d4580e8fb01d806c4092de79962de5bbb34c78e
Hsxxnil <lhsuan1225@gmail.com>
2023-08-04 16:49:18 +0800

    fix: #89 lead historical_records bug

--- internal/interactor/manager/lead/lead_manager.go
+++ internal/interactor/manager/lead/lead_manager.go
@@ -210,18 +210,6 @@ func (m *manager) Update(trx *gorm.DB, input *leadModel.Update) (int, any) {
 		helpers.AddHistoricalRecord(&records, "修改", "描述為", *input.Description)
 	}
 
-	if input.Source != nil && *input.Source != *leadBase.Source {
-		helpers.AddHistoricalRecord(&records, "修改", "來源為", *input.Source)
-	} else if input.Source == nil && leadBase.Source != nil {
-		helpers.AddHistoricalRecord(&records, "移除", "來源", "")
-	}
-
-	if input.Rating != nil && *input.Rating != *leadBase.Rating {
-		helpers.AddHistoricalRecord(&records, "修改", "分級為", *input.Rating)
-	} else if input.Rating == nil && leadBase.Rating != nil {
-		helpers.AddHistoricalRecord(&records, "清除", "分級", "")
-	}
-
 	if input.Source != nil {
 		if *input.Source != *leadBase.Source {
 			if *input.Source == "" {
