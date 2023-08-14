868fed8a8f596ecffb51427aeab19abde3882651
Hsxxnil <lhsuan1225@gmail.com>
2023-08-14 11:39:01 +0800

    fix: add helpers function's annotation

--- internal/interactor/helpers/historical_record.go
+++ internal/interactor/helpers/historical_record.go
@@ -4,6 +4,7 @@ import (
 	historicalRecordModel "app.eirc/internal/interactor/models/historical_records"
 )
 
+// AddHistoricalRecord is Helper function to create historical_record.
 func AddHistoricalRecord(input *[]historicalRecordModel.AddHistoricalRecord, action, field, value string) {
 	*input = append(*input, historicalRecordModel.AddHistoricalRecord{
 		Actions: action,
